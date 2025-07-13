9930794ba73bf72414a4fe49d0b5b2332d7b6d88
Mohi <e.mohamadhosein@gmail.com>
2025-07-13 17:37:55 +0330

    completing the index and show pages

---
+++ resources/js/pages/Listing/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div 
+        v-for="listing in listings"
+        :key="listing.id"
+    >
+        <Link :href="`/listing/${listing.id}`">
+            {{ listing.city }}
+        </Link>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        listings: Array,
+    })
+</script>
