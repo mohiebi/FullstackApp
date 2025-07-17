0168ae90bb6fa3746c18104c37743010f655bf66
Mohi <e.mohamadhosein@gmail.com>
2025-07-17 18:13:33 +0330

    adding some classes to boxes on main page

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -1,14 +1,16 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <Link :href="route('listing.show', listing.id)">
-            <ListingAddress :listing="listing" />
-        </Link>
-        <div>
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <Link :href="route('listing.show', listing.id)">
+                <ListingAddress :listing="listing" />
+            </Link>
+            <div>
+                <Link :href="route('listing.edit', listing.id)">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
+            </div>
+        </Box>
     </div>
 </template>
 
@@ -24,6 +26,7 @@ defineProps({
 
 <script>
 import MainLayout from '@/layouts/MainLayout.vue';
+import Box from '@/components/ui/Listings/Box.vue';
 
 export default {
     layout: MainLayout
