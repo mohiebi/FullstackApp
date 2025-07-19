9934fee332bf29de6b6cb960d36c9d41555a8207
Mohi <e.mohamadhosein@gmail.com>
2025-07-19 09:18:02 +0330

    using new data in show page

--- resources/js/pages/Listing/Show.vue
+++ resources/js/pages/Listing/Show.vue
@@ -21,13 +21,32 @@
                     <label class="label">Interest rate ({{ interestRate }}%)</label>
                     <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
                         class="price-range" />
-
                     <label class="label">Duration ({{ duration }} years)</label>
                     <input v-model.number="duration" type="range" min="3" max="35" step="1" class="price-range" />
 
                     <div class="text-gray-600 dark:text-gray-300 mt-2">
                         <div class="text-gray-400">Your monthly payment</div>
                         <Price :price="monthlyPayment" class="text-3xl" />
+                    </div>
+                    <div class="mt-2 text-gray-500">
+                        <div class="flex justify-between">
+                            <div>Total paid</div>
+                            <div>
+                                <Price :price="totalPaid" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Principal paid</div>
+                            <div>
+                                <Price :price="listing.price" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Interest paid</div>
+                            <div>
+                                <Price :price="totalInterest" class="font-medium" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Box>
@@ -49,7 +68,7 @@ const duration = ref(25);
 const props = defineProps({
     listing: Object,
 });
-const monthlyPayment = useMonthlyPayment(props.listing.price, interestRate, duration);
+const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(props.listing.price, interestRate, duration);
 
 </script>
 
