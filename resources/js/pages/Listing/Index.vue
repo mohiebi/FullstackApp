74a8d3a1bd86ffc7369d7041703e2ce97caf00bf
Mohi <e.mohamadhosein@gmail.com>
2025-07-19 09:04:38 +0330

    adding monthly price to index page

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -1,24 +1,13 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <Link :href="route('listing.show', listing.id)">
-                <Price :price="listing.price" class="text-2xl font-bold" />
-                <ListingSpace :listing="listing" class="text-lg" />
-                <ListingAddress :listing="listing" class="text-gray-500" />
-            </Link>
-            <div>
-                <Link :href="route('listing.edit', listing.id)">Edit</Link>
-            </div>
-            <div>
-                <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
-            </div>
-        </Box>
+        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
     </div>
 </template>
 
 <script setup>
-import ListingAddress from '@/components/ui/Listings/ListingAddress.vue';
-import { Link } from '@inertiajs/vue3';
+import MainLayout from '@/layouts/MainLayout.vue';
+import Listing from '@/pages/Listing/Components/Listing.vue';
+
 
 defineProps({
     listings: Array,
@@ -27,10 +16,6 @@ defineProps({
 </script>
 
 <script>
-import MainLayout from '@/layouts/MainLayout.vue';
-import Box from '@/components/ui/Listings/Box.vue';
-import ListingSpace from '@/components/ui/Listings/ListingSpace.vue';
-import Price from '@/components/ui/Listings/Price.vue';
 
 export default {
     layout: MainLayout
