cdae68fb88edb35c46f33c2b295ce1688fce1f8b
Mohi <e.mohamadhosein@gmail.com>
2025-07-20 14:31:39 +0330

    adding isAdmin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,15 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Mohi',
             'email' => 'e.mohamadhosein@gmail.com',
+            'is_admin' => true,
         ]);
+
+        User::factory(5)->create();
 
         \App\Models\Listing::factory(20)->create([
             'by_user_id' => 1
+        ]);
+
+        \App\Models\Listing::factory(15)->create([
+            'by_user_id' => 2
         ]);
     }
 }
