a1189982f6b2d2f4e0f77cd8bb69c7314f4a5e0c
Mohi <e.mohamadhosein@gmail.com>
2025-07-20 19:12:44 +0330

    adding pagination component and use it

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
+        <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
+    </div>
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-8">
+        <Pagination :links="listings.links" />
     </div>
 </template>
 
 <script setup>
+import Pagination from '@/components/ui/Pagination.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
 import Listing from '@/pages/Listing/Components/Listing.vue';
 
 
 defineProps({
-    listings: Array,
+    listings: {
+        type: Object,
+        required: true,
+        // validator: val => Array.isArray(val.data) && val.links
+    }
 });
-
 </script>
 
 <script>
