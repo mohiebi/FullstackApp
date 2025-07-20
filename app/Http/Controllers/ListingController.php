02bed106ec67365af29e0611e781abdf37d0fcd3
Mohi <e.mohamadhosein@gmail.com>
2025-07-20 11:55:16 +0330

    adding relation while creating nwe listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ListingController extends Controller
 {
@@ -32,7 +33,8 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        Listing::create(
+        // $request->user()->listings()->create()
+        Auth::user()->listings()->create(
             $request->validate([
                 'beds' => 'required|integer|min:1|max:20',
                 'baths' => 'required|integer|min:1|max:20',
