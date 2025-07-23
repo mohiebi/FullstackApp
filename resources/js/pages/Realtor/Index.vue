f54c0032013b5397ae82e99b9e8101ffa3b823aa
Mohi <e.mohamadhosein@gmail.com>
2025-07-23 11:31:25 +0330

    adding functionality to filter

--- resources/js/pages/Realtor/Index.vue
+++ resources/js/pages/Realtor/Index.vue
@@ -1,6 +1,8 @@
 <template>
     <h1 class="text-3xl mb-4">Your Listings</h1>
-    <section class="mb-8">Filters</section>
+    <section class="mb-8">
+        <RealtorFilters />
+    </section>
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
         <Box v-for="listing in listings" :key="listing.id">
             <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
@@ -30,6 +32,7 @@ import ListingSpace from '@/components/ui/Listings/ListingSpace.vue';
 import Price from '@/components/ui/Listings/Price.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
 import { Link } from '@inertiajs/vue3'
+import RealtorFilters from './components/RealtorFilters.vue';
 defineProps({
     listings: Array,
 });
