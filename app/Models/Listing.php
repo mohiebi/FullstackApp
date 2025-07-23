5ee1c32f2bad4cd1c56425f35c4febe1dca34b49
Mohi <e.mohamadhosein@gmail.com>
2025-07-23 09:48:35 +0330

    adding soft delete to listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
     /** @use HasFactory<\Database\Factories\ListingFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'beds',
