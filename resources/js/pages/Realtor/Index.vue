e0c64a51b6242d2ef632faa9f41a81e5ae16492a
Mohi <e.mohamadhosein@gmail.com>
2025-07-24 20:48:28 +0330

    fixing the edit button

--- resources/js/pages/Realtor/Index.vue
+++ resources/js/pages/Realtor/Index.vue
@@ -15,7 +15,8 @@
                     <ListingAddress :listing="listing" />
                 </div>
                 <div class="flex items-center gap-1 text-gray-600 dark:text-gray-300">
-                    <Link class="btn-outline text-xs font-medium">Preview</Link>
+                    <a class="btn-outline text-xs font-medium" :href="route('listing', listing)"
+                        target="_blank">Preview</a>
                     <Link class="btn-outline text-xs font-medium">Edit</Link>
                     <Link class="btn-outline text-xs font-medium" :href="route('realtor.listing.destroy', listing)"
                         as="button" method="delete">Delete</Link>
