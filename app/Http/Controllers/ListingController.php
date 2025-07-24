10107a223ca95feb7c8c03b096d6d448fcf0ec2d
Mohi <e.mohamadhosein@gmail.com>
2025-07-24 20:48:39 +0330

    moving the controllers

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -91,13 +91,7 @@ class ListingController extends Controller
      */
     public function edit(Listing $listing)
     {
-        Gate::authorize('update', $listing);
-        return inertia(
-            'Listing/Edit',
-            [
-                'listing' => $listing
-            ]
-        );
+        //
     }
 
     /**
@@ -105,22 +99,7 @@ class ListingController extends Controller
      */
     public function update(Request $request, Listing $listing)
     {
-        Gate::authorize('update', $listing);
-        $listing->update(
-            $request->validate([
-                'beds' => 'required|integer|min:0|max:20',
-                'baths' => 'required|integer|min:0|max:20',
-                'area' => 'required|integer|min:15|max:1500',
-                'city' => 'required',
-                'code' => 'required',
-                'street' => 'required',
-                'street_nr' => 'required|min:1|max:1000',
-                'price' => 'required|integer|min:1|max:20000000',
-            ])
-        );
-
-        return redirect()->route('listing.index')
-            ->with('success', 'Listing was changed!');
+        //
     }
 
     /**
