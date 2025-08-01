37374a16b9f0c370e7db5a3de887180395b6dc8b
Mohi <e.mohamadhosein@gmail.com>
2025-08-01 13:22:08 +0330

    create a notification OfferMade and use it to store data in DB

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Offer;
+use App\Notifications\OfferMade;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
@@ -14,12 +15,15 @@ class ListingOfferController extends Controller
     public function store(Listing $listing, Request $request): RedirectResponse
     {
         Gate::authorize('view', $listing);
-        $listing->offers()->save(
+        $offer = $listing->offers()->save(
             Offer::make(
                 $request->validate([
                     'amount' => 'required|integer|min:1|max:50000000'
                 ])
             )->bidder()->associate($request->user())
+        );
+        $listing->owner->notify(
+            new OfferMade($offer)
         );
 
         return redirect()->back()->with(
