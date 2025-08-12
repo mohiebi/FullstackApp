5fa2e4cece0a7da6ddb85f6ab506c3b00c9842cb
Mohi <e.mohamadhosein@gmail.com>
2025-08-12 16:56:01 +0330

    export types

---
+++ resources/types/generated.d.ts
@@ -0,0 +1,32 @@
+declare namespace App.DTO {
+    export type ListingData = {
+        id: number;
+        beds: number;
+        baths: number;
+        area: number;
+        city: string;
+        code: string;
+        street: string;
+        price: number;
+        owner: App.DTO.UserData;
+        images: Array<any>;
+        offerMade: App.DTO.OfferData | null;
+    };
+    export type ListingImageData = {
+        id: number;
+        filename: string;
+        src: string;
+    };
+    export type OfferData = {
+        id: number;
+        amount: number;
+        accepted_at: string | null;
+        rejected_at: string | null;
+        bidder: App.DTO.UserData | null;
+    };
+    export type UserData = {
+        id: number;
+        name: string;
+        email: string | null;
+    };
+}
